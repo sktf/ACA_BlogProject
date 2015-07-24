f4a5c5fffc966bc76c0aafad402cdcf4fce26075
Susan <sktf@live.com>
2015-07-23 20:21:28 -0500

    slight form adjustments

--- edit_post.php
+++ edit_post.php
@@ -1,20 +1,18 @@
 <?php
-
+//note: read map generator files and look for syntax, close to what is needed
 include('db_info.php');
 
-
+//Name="text_body"
 ?>
 
-<h1>Blog One</h1>
+<h1>Blog Blog Blog</h1>
 
 <form action="edit_post.php" method="POST">
 
+    <input type="submit" name="submit" value="SUBMIT" /><br>
+    <br><br>
     <input type="text" name="author" value="Author" /><br>
     <br>
-    <input type="text" name="text_body" value="Enter Blog" /><br>
-    <br>
+    <TEXTAREA name="blog_body" COLS="20" ROWS="4" </TEXTAREA>
 
-    <br><br><br>
-    <input type="submit" name="submit" value="SUBMIT" /><br>
-
-    </form>
+</form>
